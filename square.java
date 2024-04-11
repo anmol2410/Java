f46a0fddc30b35493bed1dd32422422698ad3e69
Anmol Gupta <anmolg2411@gmail.com>
2024-04-11 04:02:42 +0000

    pro 5

--- square.java
+++ square.java
@@ -4,6 +4,16 @@
 import java.util.Scanner;
 
 public class square {
-
-    
+    public static void main(String s[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter side of square: ");
+        int a = sc.nextInt();
+        int res =new1.area(a);
+        System.out.println("Area of square : "+ res);
+    }
+}
+class new1{
+    static int area(int a){
+        return a*a;
+    }
 }
